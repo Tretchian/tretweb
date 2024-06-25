33f8e3f5aa536e47c8296184558bc5037212d3a6
Tretchian <safonovila7@gmail.com>
2024-06-26 02:30:22 +0300

    placeholder

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -10,6 +10,10 @@ export default function Body(){
         </div>
 
         <div className='maincolumn'>
+            <div className='searchtab'>
+            <img src='img/bottom part.png' width={805} height={46}></img>
+            </div>
+
             <img src='img/main screen.png' width={805} height={287}></img>
             <img src='img/card.png' width={805} height={491}></img>
         </div>
