00b3bf5f46c42b0ad3f8e14d2bb6676656b6db28
Tretchian <safonovila7@gmail.com>
2024-06-25 17:36:54 +0300

    header alpha complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,30 @@
 import './Header.css'
-
 export default function Header(){
     return(
       <header>
-        <rect className='headrect'>
-            <img src="./img/logo.svg" alt = ''></img>
+        <div className='headrect'>
+          <div className='head'>
             <div className='headliner'>
+              <img src='img/logo.svg' alt = 'logo.png'></img>
               <a href='/' className='headlink'>Главная</a>
               <a href='/' className='headlink'>О компании</a>
               <a href='/' className='headlink'>Проекты</a>
               <a href='/' className='headlink'>Новости</a>
               <a href='/' className='headlink'>Отзывы и сертификаты</a>
-              <a href='/' className='headlink'>Контакты</a></div>
-            <div className='headliner'>Москва</div>
-            <div className='headliner'>Пн-Пт с 8:00-18:00</div>
-            <div className='headliner'>promair@mail.ru</div>
-            <div className='headliner'>тг и ватс</div>
-            <div className='headliner'>+7 (900) 111-22-33</div>
-        </rect>
+              <a href='/' className='headlink'>Контакты</a>
+              <div className='headlink'>
+              <div>
+              <img src='img/pin.svg'></img>Москва
+              </div>
+            </div> 
+            <div className='headliner'>
+              <div>
+                <img src='img/phone.svg'></img>+7 (900) 111-22-33</div> 
+              </div>
+          </div>
+            </div>
+            
+         </div>
       </header>
     )
   }
