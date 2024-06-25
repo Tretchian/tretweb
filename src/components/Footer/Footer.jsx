1c4d63f7b13d979c7acfc5e2a6f68cf1b7eca7a1
Tretchian <safonovila7@gmail.com>
2024-06-26 01:07:02 +0300

    Footer indev

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,38 @@ export default function Footer(){
     return(
         <div className='footer'>
             <div className='top_part'>
+                <div className='top_card'>
 
+                    <img src='img/logo.svg' alt='logo' className='logo'></img>
+
+                    <div>
+                    <img src='img/logos/Facebook.svg' alt='fcb' className='top_svg'></img>    
+                    <img src='img/logos/Instagram.svg' alt='ins' className='top_svg'></img>    
+                    <img src='img/logos/Telegram.svg' alt='tg' className='top_svg'></img>    
+                    <img src='img/logos/VK.svg' alt='vk' className='top_svg'></img>    
+                    </div>
+
+                </div>
+                <div className='top_card'>
+
+                    <a href='/' className='footerlink'>Главная</a>
+                    <a href='/' className='footerlink'>О компании</a>
+                    <a href='/' className='footerlink'>Проекты</a>
+
+                </div>
+                <div className='top_card'>
+
+                    <a href='/' className='footerlink'>Новости</a>
+                    <a href='/' className='footerlink'>Отзывы и сертификаты</a>
+                    <a href='/' className='footerlink'>Контакты</a>
+
+                </div>
+                <div className='top_card'>
+
+                    
+
+                </div>
             </div>
-
         </div>
     )
 }
